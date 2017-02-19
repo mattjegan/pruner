0501e2e3e0d4a5238042bb4a679a4f9d4e2995b0
mattjegan <matthewj.egan@hotmail.com>
2017-02-19 21:59:04 +1100

    add test for --help

--- pruner/tests/test_pruner.py
+++ pruner/tests/test_pruner.py
@@ -1,9 +1,17 @@
-from mock import patch
 
+import sys
+import unittest
+
+from mock import patch
 import pytest
 
 from pruner import main
 
-class TestRanCat:
-    def test_basic_run(self):
-        pass
+class TestRanCat(unittest.TestCase):
+    def test_help(self):
+        args = ['pruner', '--help']
+        with self.assertRaises(SystemExit) as sysex:
+            with patch.object(sys, 'argv', args):
+                main()
+
+        assert sysex.exception.code == 0
