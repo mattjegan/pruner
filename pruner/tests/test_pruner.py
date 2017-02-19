c5b160d0bb67680fa060154040aff18db0bd6db6
mattjegan <matthewj.egan@hotmail.com>
2017-02-19 22:33:19 +1100

    add full run test

--- pruner/tests/test_pruner.py
+++ pruner/tests/test_pruner.py
@@ -9,9 +9,29 @@ from pruner import main
 
 class TestRanCat(unittest.TestCase):
     def test_help(self):
-        args = ['pruner', '--help']
+        args = [
+            'pruner',
+            '--help'
+        ]
         with self.assertRaises(SystemExit) as sysex:
             with patch.object(sys, 'argv', args):
                 main()
 
         assert sysex.exception.code == 0
+
+    def test_full_run(self):
+        args = [
+            'pruner',
+            'pruner/tests/fake_proj/requirements.txt',
+            'pruner/tests/fake_proj/output.txt',
+            'python', 'pruner/tests/fake_proj/fake_proj.py'
+        ]
+        try:
+            with patch.object(sys, 'argv', args):
+                main()
+        except:
+            assert 1 == 0
+
+        with open('pruner/tests/fake_proj/output.txt') as f:
+            topline = f.readline()
+            assert topline.startswith('rancat') == True
